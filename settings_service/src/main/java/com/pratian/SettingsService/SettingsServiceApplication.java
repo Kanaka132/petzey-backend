83edb3cffba6ae0c03ac9e6e365f9d1ba5f92a0a
Kanaka132 <92907518+Kanaka132@users.noreply.github.com>
2022-11-24 15:52:53 +0530

    Add files via upload

---
+++ settings_service/src/main/java/com/pratian/SettingsService/SettingsServiceApplication.java
@@ -0,0 +1,28 @@
+package com.pratian.SettingsService;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SettingsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettingsServiceApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+}
